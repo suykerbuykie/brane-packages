aca115a6b19e33dd00a06ee7094f16d011e7202e
Edgardo Reinoso <e.reinoso@azerion.com>
2021-05-27 08:48:36 +0200

    modified the weather package

--- weather/run.py
+++ weather/run.py
@@ -3,6 +3,7 @@ import os
 import sys
 import yaml
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 import matplotlib.dates as md
 import requests
 
@@ -13,12 +14,20 @@ def graph(city: str, file: str) -> str:
 
 	dts = [x['dt_txt'] for x in weatherInfo['list']]
 	y_val = [x['main']['temp'] for x in weatherInfo['list']]
+	plt.figure(figsize=(17, 8))
 	plt.subplots_adjust(bottom=0.3)
-	plt.text(0.9, 0.9, f"Weather for: {weatherInfo['city']['name']}", size=8, color='black')
-	plt.xticks(rotation=90)
-	plt.tick_params(axis='x', which='minor', labelsize=8)
+			# x, y
+	# plt.title(11, 23, f"Weather for: {weatherInfo['city']['name']}", size=10, color='black')
+	plt.title(f"Weather for: {weatherInfo['city']['name']}")
+	plt.ylabel('Temperature C')
+	plt.xticks(rotation=45)
+	plt.tick_params(axis='x', which='minor', labelsize=15)
+	print('y_val: ',y_val)
+	print('dts: ',dts)
 	plt.plot(dts, y_val)
+	# plt.show()
 	plt.savefig(file)
+	# ax.savefig(file)
 
 	return file
 
