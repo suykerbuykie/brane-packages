2769e020e70ad857b84789aa8771f90802df63fc
Tom Suijkerbuijk <tomtomsuykerbuyk@gmail.com>
2021-06-01 21:48:33 +0200

    remove apikey and add comments and readme

---
+++ weather/readme.md
@@ -0,0 +1,9 @@
+# Weather Brane package
+
+`$ brane import suykerbuykie/brane-packages/weather`
+
+The package takes 3 parameters:
+- City Name (e.g. Amsterdam)
+- apiKey (can be requested for free on https://openweathermap.org/api)
+- file path, where the .png will be saved.
+
--- weather/run.py
+++ weather/run.py
@@ -3,31 +3,28 @@ import os
 import sys
 import yaml
 import matplotlib.pyplot as plt
-from matplotlib.pyplot import figure
-import matplotlib.dates as md
 import requests
 
-
-def graph(city: str, file: str) -> str:
-	data = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid=e372c502e24c89544b4d0f938b65ca84")
+def graph(city: str, apikey: str, file: str) -> str:
+	# GET request to openWeatherMap API for fetching the coming 5 days per 3 hours (40 datapoints) API-key can be requested for free on https://openweathermap.org/api
+	data = requests.get(f"https://api.openweathermap.org/data/2.5/forecast?q={city}&units=metric&appid={apikey}")
 	weatherInfo = data.json()
 
+	# map the x and y axis with the weatherInfo
 	dts = [x['dt_txt'] for x in weatherInfo['list']]
 	y_val = [x['main']['temp'] for x in weatherInfo['list']]
+
+	# adjusting figure size to fit the 40 datapoints and add labels to axis.
 	plt.figure(figsize=(17, 8))
 	plt.subplots_adjust(bottom=0.3)
-			# x, y
-	# plt.title(11, 23, f"Weather for: {weatherInfo['city']['name']}", size=10, color='black')
 	plt.title(f"Weather for: {weatherInfo['city']['name']}")
 	plt.ylabel('Temperature C')
 	plt.xticks(rotation=45)
 	plt.tick_params(axis='x', which='minor', labelsize=15)
-	print('y_val: ',y_val)
-	print('dts: ',dts)
+
+	# plot the graph and save it to file path.
 	plt.plot(dts, y_val)
-	# plt.show()
 	plt.savefig(file)
-	# ax.savefig(file)
 
 	return file
 
@@ -36,9 +33,10 @@ if __name__ == "__main__":
   command = sys.argv[1]
   argument_file = os.environ["FILE"]
   argument_city = os.environ["CITY"]
+  argument_key = os.environ["APIKEY"]
 
   functions = {
     "graph": graph,
   }
-  output = functions[command](argument_city, argument_file)
+  output = functions[command](argument_city, argument_key, argument_file)
   print(yaml.dump({"output": output}))
