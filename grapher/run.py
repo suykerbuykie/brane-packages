be77ff58064262fa52b349607ee9515bed5101d3
Sytse Groenwold <Sytse.Groenwold@ing.com>
2021-06-02 21:30:07 +0200

    no idea what's in this commit

--- grapher/run.py
+++ grapher/run.py
@@ -4,33 +4,11 @@ import json
 import gensim
 
 if __name__ == "__main__":
-    with open('data_50k.json') as f:
+    with open('data_500k.json') as f:
         data = json.load(f)
 
     # This is the clean corpus.
     doc_clean = [doc.split() for doc in data.values()]
-
-    # for doc in doc_clean:
-    #     print(doc)
-    #     print()
-
-    # Create dictionary and corpus
-    # dictionary = corpora.Dictionary(doc_clean)
-    # doc_term_matrix = [dictionary.doc2bow(text) for text in doc_clean]
-
-    # # Creating the object for LDA model using gensim library (code copied from assignment)
-    # Lda = gensim.models.ldamodel.LdaModel
-
-    # # Running and Trainign LDA model on the document term matrix. (code copied from assignment, changes # of passes to 2 instead of 100 to speed things up)
-    # ldamodel = Lda(doc_term_matrix, num_topics=2, id2word=dictionary, passes=2)
-    
-    # Print 2 topics and describe then with 4 words. (code copied from assignment)
-    # topics = ldamodel.print_topics(num_topics=5, num_words=4)
-
-    # i=0
-    # for topic in topics:
-    #     print ("Topic",i ,"->", topic)     
-    #     i+=1
 
     # Create dictionary out of clean data
     dictionary = gensim.corpora.Dictionary(doc_clean)
